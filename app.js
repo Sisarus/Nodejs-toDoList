30a5393cc64ec092cbdc51f1c4e638f079e094de
Alisa <alisa.luoma@hotmail.fi>
2023-02-09 13:43:03 +0200

    To do list - can delete

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const port = 3000;
 
 //parse application
 app.use(bodyParser.urlencoded({extended: true}));
+// Express use local files in public
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs')
 
 // To do Items
 
 var items = [];
+var workItems = [];
 
 
 app.get("/", (req, res) => {
@@ -26,17 +29,44 @@ app.get("/", (req, res) => {
 
   var day = today.toLocaleDateString("en-GB", options);
 
-  res.render('list', {kinfOfDay: day, newListItems: items});
+  res.render('list', {listTitle: day, newListItems: items});
 });
 
 app.post('/', (req, res)=> {
   var item = req.body.newItem;
 
-  items.push(item);
-  
-  res.redirect("/");
+  if(req.body.list === "Work"){
+    if(item.length != 0){
+      workItems.push(item);
+    }
+    res.redirect("/work");
+  } else{
+    if(item.length != 0){
+      items.push(item);
+    }
+    res.redirect("/");
+  }
 });
 
+app.post("/del", (req, res)=>{
+  var deleteItem = req.body.del;
+
+  if(req.body.list === "Work"){
+    workItems.splice(deleteItem, 1);
+    res.redirect("/work");
+  } else {
+    items.splice(deleteItem, 1);
+    res.redirect("/");
+  }
+});
+
+app.get("/about", (req, res)=>{
+  res.render('about');
+});
+
+app.get("/work", (req, res) => {
+  res.render('list', {listTitle: "Work List", newListItems: workItems});
+});
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
